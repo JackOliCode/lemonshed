9fdaa0ddbbc7f6f84afaba0ba70fdaa4b0bc1c04
JackOliCode <jack.v.oliver@gmail.com>
2023-10-01 15:34:30 +0100

    Update 0003_recipe_description_recipe_pic_recipe_type.py

--- recipes/migrations/0003_recipe_description_recipe_pic_recipe_type.py
+++ recipes/migrations/0003_recipe_description_recipe_pic_recipe_type.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='recipe',
             name='type',
-            field=models.CharField(choices=[('breakfast', 'Breakfast'), ('lunch', 'Lunch'), ('dinner', 'Dinner'), ('snack', 'Snack'), ('drink', 'Drink')], default='Other', max_length=40),
+            field=models.CharField(choices=[('breakfast', 'Breakfast'), ('lunch', 'Lunch'), ('dinner', 'Dinner'), ('snack', 'Snack'), ('drink', 'Drink')], default='Snack', max_length=40),
         ),
     ]
